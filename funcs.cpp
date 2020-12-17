017f8daed04e6608ab1a9b10ed0af9eae87d946a
Anton <anton.stepanenkov123@gmail.com>
2020-12-17 03:35:16 +0300

    add function MyMul() and MyDiv() wtih checking

--- funcs.cpp
+++ funcs.cpp
@@ -9,3 +9,16 @@ double MySub(double a, double b)
 {
 	return a - b;
 }
+double MyMul(double a, double b)
+{
+    return a * b; 
+}
+double MyDiv(double a, double b)
+{
+	if (b != 0) {
+		return a / b;
+	}
+ else 
+	cout << "На ноль делить нельзя!!";
+
+}
