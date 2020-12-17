143c1e154dee114fb12ca81698a07e913dbb062d
Anton <anton.stepanenkov123@gmail.com>
2020-12-17 04:49:59 +0300

    hotfix 2

--- funcs.cpp
+++ funcs.cpp
@@ -1,11 +1,24 @@
-#include "funcs.h"
+﻿#include "funcs.h"
 #include <iostream>
 using namespace std;
 double MySum(double a, double b)
 {
-    return a + b;
+	return a + b;
 }
 double MySub(double a, double b)
 {
 	return a - b;
+}
+double MyMul(double a, double b)
+{
+	return a * b;
+}
+double MyDiv(double a, double b)
+{
+	if (b != 0) {
+		return a / b;
+	}
+	else
+		cout << "На ноль делить нельзя!";
+
 }
