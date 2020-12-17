b89c797c17133e83d002f563ed78b1243b9c57f0
Ksenia <ksusha.senkevich@yandex.ru>
2020-12-17 03:54:16 +0300

    adding menu in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,25 @@
 using namespace std;
 int main()
 {
+setlocale(LC_ALL, "Russian");
+	double a, b;
+	int x, operation, s;
+	do{
+		cout << "Введите число a: ";
+		cin >> a;
+		cout << "Введите число b: ";
+		cin >> b;
+
+		cout << "Выберите операцию для выполнения:" << endl;
+		cout << "1. Сложение" << endl;
+		cout << "2. Вычитание" << endl;
+		cout << "3. Умножение" << endl;
+		cout << "4. Деление" << endl;
+		cout << "0. Выход из программы" << endl;
+		cout << "Ввод операции: ";
+		cin >> operation;
+
+
+
 return 0;
 }
